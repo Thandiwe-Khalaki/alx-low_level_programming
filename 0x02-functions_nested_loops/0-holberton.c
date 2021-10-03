023ee97ea82e7d05bcbdf5b0a50deca48a5033df
Thandiwe-Khalaki <thandily@gmail.com>
2021-10-03 19:47:44 +0200

    fixed files

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints Holberton as a message using putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "Holberton";
+
+int i;
+
+for (i = 0; i <= str[i]; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+
+return (0);
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * wildcmp - check the code for Holberton School students.
+ * @s1: string
+ * @s2: string
+ * Return: Always 0.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
